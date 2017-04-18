e23e29b1d8fdd479de94da2d6bcf44fe709d9a5a
mordohaw <william.mordohay@gmail.com>
2017-04-18 10:55:55 +0200

    18/04 Add an AlertDialog when the user quit the application and add the resume what i did

--- MelodieAndroidV4.4/app/src/main/java/com/example/williammordohay/melodieandroidv44/Menu.java
+++ MelodieAndroidV4.4/app/src/main/java/com/example/williammordohay/melodieandroidv44/Menu.java
@@ -1,5 +1,7 @@
 package com.example.williammordohay.melodieandroidv44;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -20,7 +22,23 @@ public class Menu extends AppCompatActivity {
     }
 
     public void quitCurrentActivity(View v){
-        Menu.this.finish();
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(R.string.check_dialog_message)
+                .setTitle(R.string.check_dialog_title)
+                .setPositiveButton(R.string.Yes, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // CONFIRM
+                        Menu.this.finish();
+                    }
+                })
+                .setNegativeButton(R.string.No, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // CANCEL
+                    }
+                });
+        // Create the AlertDialog object
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 
     public void goToFirstActivity(View v){
